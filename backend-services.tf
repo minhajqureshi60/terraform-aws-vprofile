f8fefe1152a07d963e881ce393fabac24dcd2871
minhaj qureshi <minhajqureshi60@gmail.com>
2023-04-03 15:30:41 +0530

    backend services

--- backend-services.tf
+++ backend-services.tf
@@ -18,6 +18,7 @@ resource "aws_db_instance" "vprofile-rds" {
   engine                 = "mysql"
   engine_version         = "8.0.32"
   instance_class         = "db.t2.micro"
+  name                   = var.dbname
   username               = var.dbuser
   password               = var.dbpass
   parameter_group_name   = "default.mysql8.0"
@@ -42,7 +43,7 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
 resource "aws_mq_broker" "vprofile-rmq" {
   broker_name        = "vprofile-rmq"
   engine_type        = "ActiveMQ"
-  engine_version     = "5.15.0"
+  engine_version     = "5.17.2"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.vprofile-backend-sg.id]
   subnet_ids         = [module.vpc.private_subnets[0]]
